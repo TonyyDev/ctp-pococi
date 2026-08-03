1f9a1debbf34b2f498e3bc709b6739836588a6b9
Anthony Barquero <thony.dev@hotmail.com>
2026-08-03 09:16:07 -0600

    loginpage added

--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -4,11 +4,13 @@ import AdminPage from "../pages/AdminPage";
 import DashboardPage from "../pages/DashboardPage";
 import BecasPage from "../pages/BecasPage";
 import NotFound404 from "../pages/NotFound404";
+import LoginPage from "../auth/LoginPage";
 
 export const RoutesTree = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/admin" element={<AdminPage />}>
         <Route index element={<Navigate to="dashboard" replace />} />
         <Route path="dashboard" element={<DashboardPage />} />
